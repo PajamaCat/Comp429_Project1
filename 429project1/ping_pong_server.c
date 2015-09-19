5e341d9ecd53d5a63c29d4f714117f88bb0ba40b
Jiafang Jiang <jiafang.jiang@gmail.com>
2015-09-19 11:02:59 -0500

    copied from class code

--- 429project1/ping_pong_server.c
+++ 429project1/ping_pong_server.c
@@ -6,4 +6,13 @@
 //  Copyright © 2015 jj26&wz19. All rights reserved.
 //
 
+#include <errno.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <fcntl.h>
