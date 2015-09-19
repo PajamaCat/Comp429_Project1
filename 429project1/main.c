622c34fa0ffacaa46fdff163ca927b3230fdf71d
Jiafang Jiang <jiafang.jiang@gmail.com>
2015-09-19 10:51:36 -0500

    Initial Commit

---
+++ 429project1/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  429project1
+//
+//  Created by Fang on 9/19/15.
+//  Copyright © 2015 jj26&wz19. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
